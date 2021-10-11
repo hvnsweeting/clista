72e87d748ab44eeda8cae6854f83052257c4cea0
Viet Hung Nguyen <hvn@familug.org>
2021-10-11 23:22:34 +0700

    Accept arguments, read from stdin

--- src/clist/__init__.py
+++ src/clist/__init__.py
@@ -1,22 +1,33 @@
-STREAMLIT_APP = """
-import subprocess
-import streamlit as st
-opts = []
-"""
+import argparse
+import sys
+
 __version__ = (0, 0, 1)
 __version_str__ = ".".join(str(i) for i in __version__)
 
 
 def main():
-    print("CLIST started")
     # write st app dynamically or gen code?
     # use template or AST?
+    argp = argparse.ArgumentParser()
+    argp.add_argument("--sample", action="store_true", help="Generate sample program from `hey` CLI tool")
+    args = argp.parse_args()
+    if args.sample:
+        import clist.hey_help as hey_help
 
+        generate_streamlit_app(hey_help.text.strip())
+    else:
+        generate_streamlit_app(sys.stdin.read().strip())
+
+
+def generate_streamlit_app(cli_help, outfile="stapp.py.gen"):
+    STREAMLIT_APP = """
+import subprocess
+import streamlit as st
+opts = []
+"""
     stapp = [STREAMLIT_APP]
 
-    # TODO get help msg of the command via stdin
-    with open("heyhelp.txt") as f:
-        lines = [i for i in f.readlines() if i.strip()]
+    lines = [i for i in cli_help.splitlines() if i.strip()]
 
     # parse to get the command, the required arg
     # Usage: hey [options...] <url>
@@ -32,7 +43,7 @@ def main():
     for line in options:
         opt, msg = line.split(maxsplit=1)
         opt_msg[opt.strip("-")] = line.strip()
-        opt_var = opt.strip('-').replace('-', '_')
+        opt_var = opt.strip("-").replace("-", "_")
 
         stapp.append(f"opt_{opt_var} = st.text_input('{line.strip()}')")
         stapp.append(f"opts.append(('{opt}', opt_{opt_var}))")
@@ -51,6 +62,8 @@ if run:
     end = APP_END % (name)
     with open("stapp.py.gen", "wt") as f:
         f.write("\n".join(stapp + end.splitlines()))
-        print("Wrote stapp.py.gen, please copy as stapp.py, edit as needed then run streamlit run stapp.py")
+        print(
+            "Wrote stapp.py.gen, please copy as stapp.py, edit as needed then run streamlit run stapp.py"
+        )
 
     # run the app via streamlit when option passed
